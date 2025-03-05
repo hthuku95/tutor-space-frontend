4be19d015ee0fd15b6e4ddb0fe56a5ff875a29fe
Harry Thuku <harry@knabu.me>
2025-03-05 19:16:27 +0300

    Returned Bidding Button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -48,14 +48,14 @@ const Navbar = () => {
             >
               <AddIcon />
             </IconButton>
-          {/**            <IconButton
+            <IconButton
               color="inherit"
               component={RouterLink}
               to="/bidding"
               aria-label="bidding"
             >
               <GavelIcon />
-            </IconButton> */}
+            </IconButton> 
 
           </>
         )}
